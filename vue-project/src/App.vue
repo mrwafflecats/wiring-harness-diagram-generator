5bec7c8697cf6b58b8c3ac189bb35201e276fd06
mrwafflecats <slanavanh@gmail.com>
2022-10-27 11:38:37 -0500

    trimmed some router and view fat

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,29 +1,28 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img
+    <!-- <img
       alt="Vue logo"
       class="logo"
       src="@/assets/logo.svg"
       width="125"
       height="125"
-    />
+    /> -->
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav>
+      <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
+      </nav> -->
     </div>
   </header>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
