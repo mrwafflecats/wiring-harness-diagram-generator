f1200668b9f864c030955c1bd8662c3bb9c79e86
mrwafflecats <slanavanh@gmail.com>
2022-10-28 13:08:43 -0500

    added PinInput component

---
+++ vue-project/src/components/PinInput.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+    data() {
+        return{
+            id:Number,
+            pinNum:'',
+            color: 'black',
+            description: 'Add a description',
+            connection: {pinNum: '', connector: ''}
+        }
+    },
+    methods:{
+        AddPin(){
+            return
+        }
+    }
+}
+</script>
+
+<template>
+<input :value="pinNum" placeholder="pin">
+<input :value="color" placeholder={{color}}>
+<input :value="description" placeholder={{description}}>
+<button @click="AddPin">Add Pin</button>
+</template>
+
+<style>
+
+</style>
