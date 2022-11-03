7f5259c36064c3574a2d254174cf762db95abb5a
mrwafflecats <slanavanh@gmail.com>
2022-11-03 13:37:37 -0500

    added device component

---
+++ vue-project/src/components/Device.vue
@@ -0,0 +1,21 @@
+<script>
+import Connector from "./Connector.vue"
+
+export default{
+    components:{
+        Connector
+    },
+    data(){
+        return{
+            id:'',
+            name:'',
+            connectors:[]
+        }
+    }
+}
+</script>
+
+<template>
+<li v-for="connector in connectors">{{connector}}</li>
+<!-- TODO add a way to create new connectors -->
+</template>
