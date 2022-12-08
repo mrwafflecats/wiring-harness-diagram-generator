ee322d83ad568acfc1617c0222439ebaa92245f7
mrwafflecats <slanavanh@gmail.com>
2022-12-08 13:52:25 -0600

    increment pinNum 80% done, see TODO for issue

--- vue-project/src/components/PinInput.vue
+++ vue-project/src/components/PinInput.vue
@@ -9,13 +9,36 @@ export default{
             color: '',
             description: '',
             connection: {pinNum: '', connector: ''}
-        }
+        },
+        autoIncrement: true //automatically increments the pin number
     };
 },
     emits:['addPin'],
     methods:{
         AddPin(){
+            // TODO it doesn't account for a char to be before the digit and changes the char instead of the digit
+            //likely solution to be in the regex
             this.$emit('addPin', this.pin)
+            //js is witchcraft 
+            if (this.autoIncrement){
+                var str = this.pin.pinNum
+                var numIndex = str.search(/\d*/)
+                var numArray = str.match(/\d*/)
+                var digitCount = numArray.length
+                ++numArray[numArray.length-1]
+
+                if(numArray[numArray.length-1] == "0")
+                    ++numArray[numArray.length-2]                    
+                
+                if(numArray[numArray.length-2] == "0")
+                    ++numArray[numArray.length-3]                
+        //if you somehow manage to get a connector with more than 1000 pins on it, you are doing something wrong
+            var tempStr = str.split('')
+            tempStr.splice(numIndex,digitCount,numArray.toString())
+            var tempStr = tempStr.join('')
+
+            this.pin.pinNum = tempStr
+            }
         }
     }
 }
