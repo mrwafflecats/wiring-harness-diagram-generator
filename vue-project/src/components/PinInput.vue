be876b7c93dc841e66f92dcb819279a913d312d5
MrWaffleCats <slanavanh@gmail.com>
2023-01-18 02:56:03 -0600

    doesn't autoincrement before emit finishes

--- vue-project/src/components/PinInput.vue
+++ vue-project/src/components/PinInput.vue
@@ -1,48 +1,50 @@
 <script>
-export default{
+export default {
 
     data() {
-        return{
+        return {
             // id:Number
-            pin:{
-            pinNum: '',
-            color: '',
-            description: '',
-            connection: {pinNum: '', connector: ''}
-        },
-        autoIncrement: true //automatically increments the pin number
-    };
-},
-    emits:['addPin'],
-    methods:{
-        AddPin(){
-            // TODO: the pin increments before adding it, which is annoying and should be the other way around
+            pin: {
+                pinNum: '',
+                color: '',
+                description: '',
+                connection: { pinNum: '', connector: '' }
+            },
+            autoIncrement: true, // automatically increments the pin number
+        };
+    },
+    emits: ['addPin'],
+    methods: {
+        AddPin() {
             this.$emit('addPin', this.pin)
+            this.$nextTick(this.AutoIncrement)
+        },
+        AutoIncrement() {
             //js is witchcraft 
-            if (this.autoIncrement){
+            if (this.autoIncrement) {
 
                 var str = this.pin.pinNum
                 var numIndex = str.search(/\d/)
-            
+
                 var numArray = str.match(/\d*/)//does not seperate the digits, treats it as one digit
                 var digitCount = () => {
                     if (numArray >= 10)
                         return 2
                     if (numArray >= 100)
                         return 3
-                    else 
+                    else
                         return 1
                 }//if you somehow manage to get a connector with more than 1000 pins on it, you are doing something wrong
                 console.log(numIndex)
                 numArray++
-               
 
-            var tempStr = str.split('')
-            tempStr.splice(numIndex, digitCount(), numArray.toString())
-            var tempStr = tempStr.join('')
 
-            this.pin.pinNum = tempStr
+                var tempStr = str.split('')
+                tempStr.splice(numIndex, digitCount(), numArray.toString())
+                var tempStr = tempStr.join('')
 
+                this.pin.pinNum = tempStr
+                this.emitFlag = false
             }
         }
     }
@@ -50,12 +52,12 @@ export default{
 </script>
 
 <template>
-<input v-model="pin.pinNum" placeholder="pin">
-<input v-model="pin.color" placeholder="color">
-<input v-model="pin.description" placeholder="Add a description">
-<button @click="AddPin">Add Pin</button>
-<input type="checkbox" v-model="autoIncrement" name="check"/>
-<label for="check">Auto-Increment</label>
+    <input v-model="pin.pinNum" placeholder="pin">
+    <input v-model="pin.color" placeholder="color">
+    <input v-model="pin.description" placeholder="Add a description">
+    <button @click="AddPin" >Add Pin</button>
+    <input type="checkbox" v-model="autoIncrement" name="check" />
+    <label for="check">Auto-Increment</label>
 </template>
 
 <style>
