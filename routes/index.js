2e83d0761a4bbcead9715974b020c97486dc68bc
mrwafflecats <slanavanh@gmail.com>
2023-02-01 13:23:35 -0600

    added routes to db

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+const pins = require('./db/pins')
 
 /* GET home page. */
 // TODO: have this post the vue project
@@ -11,6 +15,20 @@ router.get('/', function(req, res, next) {
 //PUT makes the server modify data sent by client, creates a new thing if it doesn't exist
 //DELETE is pretty self explanatory
 
+//Gets the pins from the database and posts it somewhere...
+app.get('/pins', (req, res) => {
+  pins.getAll().then((pins) => {
+    res.json(pins)
+  })
+})
 
+app.post('/pins', (req, res) =>{
+  pins.create(req.body).then((pin) => {
+    res.json(pin)
+  }).catch((error) => {
+    res.status(500)
+    res.json(error)
+  })
+})
 
 module.exports = router;
