2d5b4cb5a2211de49b437065b0c7e7aa3a34731e
mrwafflecats <slanavanh@gmail.com>
2023-02-01 13:42:04 -0600

    added, because app.vue needs to be its own thing

---
+++ vue-project/src/components/Home.vue
@@ -0,0 +1,19 @@
+<script>
+import Device from "./components/Device.vue"
+
+//url to beckend server route
+const API_URL = "http://localhost:4000/pins";
+
+export default{
+  mounted(){
+  fetch(API_URL)
+}
+}
+</script>
+
+<template>
+    <!-- TODO make 'Add device' button -->
+    <div class="wrapper">
+        <Device></Device>
+    </div>
+</template>
