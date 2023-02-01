d4528ec069f24b15e51056cb3e4cfee4b3b120e3
mrwafflecats <slanavanh@gmail.com>
2023-02-01 12:24:02 -0600

    dependancy stuff

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// TODO: have this post the vue project
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+//get is for retrieving data
+//POST makes the server take data sent by a client 
+//PUT makes the server modify data sent by client, creates a new thing if it doesn't exist
+//DELETE is pretty self explanatory
+
+
 
 module.exports = router;
