e5abde50924313c1b62a66c305dd6380cc7bd379
MrWaffleCats <slanavanh@gmail.com>
2023-02-06 14:33:17 -0600

    removed  editmode stuff

--- vue-project/src/components/Connector.vue
+++ vue-project/src/components/Connector.vue
@@ -5,35 +5,36 @@ import PinInput from './PinInput.vue'
 //url to backend 
 const API_URL = "http://localhost:4000/pins"
 
-export default{
+export default {
     components: {
         Pin,
-        PinInput        
+        PinInput
     },
-    props:['name'],
-    data: function() {     
-        return{
-        // TODO: auto create id
-        // id: '',
-        // name:'',
-        pins: [],
-        editMode: false,
+    props: ['name'],
+    data: function () {
+        return {
+            // TODO: auto create id
+            // id: '',
+            // name:'',
+            pins: [],
         }
     },
     mounted() {
         //gets the pins from the database
-        fetch(API_URL).then(response => response.json()).then(result => {
-            this.pins = result
-        })
+        fetch(API_URL)
+            .then(response => response.json())
+            .then(result => {
+                this.pins = result
+            })
     },
-    methods:{
-        DeletePin(pinId){
+    methods: {
+        DeletePin(pinId) {
             //TODO: this will send the pin to delete to the backend
             const id = this.pins.findIndex((pin) => pin.id === pinId)
             this.pins.splice(id, 1)
         },
-
-    }}
+    }
+}
 
 </script>
 
@@ -42,11 +43,9 @@ export default{
     <!-- TODO: add way to edit connectors, add connection prop  -->
     <ul>
         <li v-for="pin in pins" :key="pin.id">
-            <Pin :id="pin.id" :init-pin-num="pin.pinNum" :init-color="pin.color" :init-description="pin.description" :edit-mode-prop="editMode" @delete="DeletePin(pin.id)"/>
+            <Pin :id="pin.id" :init-pin-num="pin.pinNum" :init-color="pin.color" :init-description="pin.description"
+                 @delete="DeletePin(pin.id)" />
         </li>
     </ul>
-
-    <button @click="editMode = !editMode">Edit Mode</button>
-    <!-- check when getting out of editmode before sending the changes to backend -->
-    <PinInput @add-pin="AddPin"/>
+    <PinInput @add-pin="AddPin" />
 </template>
