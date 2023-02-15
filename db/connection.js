07383adcf235e71b7f19054b23ce673eafd53532
mrwafflecats <slanavanh@gmail.com>
2023-02-15 11:30:48 -0600

    changed database url

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const monk = require('monk');
 const connectionString = process.env.MONGODB_URI || 'localhost/mevnStack';//this may cause probs later
-const url ='mongodb+srv://cluster0.n7jqzlc.mongodb.net/myFirstDatabase'
+const url ='mongodb+srv://mrwafflecats:Awesome360@cluster0.n7jqzlc.mongodb.net/?retryWrites=true&w=majority'
 const db = monk(url);
 
 module.exports = db;
