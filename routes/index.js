9508a5ba902a92fbc709b01faec006075b131cba
MrWaffleCats <slanavanh@gmail.com>
2023-03-06 15:00:41 -0600

    moved routes to other files

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 var router = require('express').Router();
 var bodyParser = require('body-parser')
-const pins = require('../db/Pins');
+const DataManager = require('../db/DataManager');
 
+
+let pins = require('./pins')
+let connectors = require('./connectors')
+let devices = require('./devices')
+
+router.use('/pins', pins)
+router.use('/connectors', connectors)
+router.use('/devices', devices)
 
 /* GET home page. */
 // TODO: have this post the vue project
@@ -12,51 +20,6 @@ router.get('/', function (req, res, next) {
 //POST makes the server take data sent by a client 
 //PUT makes the server modify data sent by client, creates a new thing if it doesn't exist
 //DELETE is pretty self explanatory
-
-router.use(bodyParser.urlencoded({extended: true}))
-
-router.get('/pins', (req, res) => {
-  pins.getAll().then((pins) => {
-    res.json(pins)
-  })
-  .catch((error) => {
-    res.status(500)
-    res.json(error)
-  })
-})
-
-//Updates a pin sent by client
-router.put('/pins', (req, res) => {
-  pins.updatePin(req.body)
-    .then((result) => {
-      res.status(201).json({string:'Successfully updated pin'})
-    }).catch((error) => {
-      res.status(500)
-      res.json(error)
-    })
-})
-
-//Recieves pin from a client and adds it to the database 
-router.post('/pins', (req, res) => {
-  pins.createPin(req.body)
-    .then((result) => {
-      res.status(201).json({string:'Successfully added pin'})
-    }).catch((error) => {
-      res.status(500).json(error)
-    })
-})
-
-
-router.delete('/pins', (req, res) => {
-  pins.deletePin(req.body)
-    .then((pin) => {
-      res.status(200).json({string:'Successfully deleted the pin'})
-    }).catch((error) => {
-      res.status(500)
-      res.json(error)
-    })
-})
-
 module.exports = router;
 
 //To run the server, $npm run start
