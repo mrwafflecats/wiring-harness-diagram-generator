89aeb9e8043eabe1e25973d4f1199f7c31eefe84
MrWaffleCats <slanavanh@gmail.com>
2023-03-06 15:01:21 -0600

    moved pins routes here

---
+++ routes/pins.js
@@ -0,0 +1,58 @@
+var router = require('express').Router();
+const DataManager = require('../db/DataManager');
+var bodyParser = require('body-parser')
+/* GET home page. */
+// TODO: have this post the vue project
+  //get is for retrieving data
+  //POST makes the server take data sent by a client 
+  //PUT makes the server modify data sent by client, creates a new thing if it doesn't exist
+  //DELETE is pretty self explanatory
+  
+  router.use(bodyParser.urlencoded({extended: true}))
+
+
+  router.get('/', (req, res) => {
+    DataManager.getAll().then((pins) => {
+      res.json(pins)
+    })
+    .catch((error) => {
+      res.status(500)
+      res.json(error)
+    })
+  })
+  
+  //Updates a pin sent by client
+  router.put('/', (req, res) => {
+    DataManager.updatePin(req.body)
+      .then((result) => {
+        res.status(201).json({string:'Successfully updated pin'})
+      }).catch((error) => {
+        res.status(500)
+        res.json(error)
+      })
+  })
+  
+  //Recieves pin from a client and adds it to the database 
+  router.post('/', (req, res) => {
+    DataManager.createPin(req.body)
+      .then((result) => {
+        res.status(201).json({string:'Successfully added pin'})
+      }).catch((error) => {
+        res.status(500).json(error)
+      })
+  })
+  
+  
+  router.delete('/', (req, res) => {
+    DataManager.deletePin(req.body)
+      .then((pin) => {
+        res.status(200).json({string:'Successfully deleted the pin'})
+      }).catch((error) => {
+        res.status(500)
+        res.json(error)
+      })
+  })
+  
+  
+  
+  module.exports = router;
