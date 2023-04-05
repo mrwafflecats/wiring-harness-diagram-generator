b309af5f2d1650dfd55217e9aba6acfa9f014030
mrwafflecats <slanavanh@gmail.com>
2023-04-05 11:53:59 -0500

    Moved data manging stuff to Data.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,36 +1,5 @@
 <script setup>
-import Device from "./components/Device.vue"
-
-const API_Devices = "http://localhost:4000/devices"
-
-export default{
-  data(){
-    return{
-      devices: [],
-      deviceInput: ''
-    }
-  },
-  methods:{
-    GetDevices(){
-      fetch(API_Devices)
-      .then(response => response.json)
-      .then(result => this.devices = result)
-    },
-    AddDevice(){
-      fetch(API_Devices, {
-        method: "POST",
-        body: JSON.stringify(this.deviceInput)
-      })
-    },
-    DeleteDevice(){
-
-    },
-    UpdateDevice(){
-
-    }
-    
-  }
-}
+import DataView from "./components/Data.vue"
 </script>
 
 <template>
@@ -43,13 +12,8 @@ export default{
       height="125"
     /> -->
     <div class="wrapper">
-      <ul>
-        <li v-for="d in devices :key=d.id">
-          <Device :id="d.id" :name="d.device.name"/>
-        </li>
-      </ul>
-      <input v-model="deviceInput">
-      <button @click="AddDevice()">Add Device</button>
+      <DataView></DataView>
+      <!-- TODO add the diagram view here eventually -->
     </div>
   </header>
 
