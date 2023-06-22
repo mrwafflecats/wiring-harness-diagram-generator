fd68ec2967188ba3a482fbf58f70e21021b7ff82
MrWaffleCats <slanavanh@gmail.com>
2023-06-21 19:02:25 -0500

    added and implemented

---
+++ routes/connections.js
@@ -0,0 +1,54 @@
+var router = require('express').Router();
+const DataManager = require('../db/DataManager');
+var bodyParser = require('body-parser')
+/* GET home page. */
+  //get is for retrieving data
+  //POST makes the server take data sent by a client 
+  //PUT makes the server modify data sent by client, creates a new thing if it doesn't exist
+  //DELETE is pretty self explanatory
+  
+  router.use(bodyParser.urlencoded({extended: true}))
+
+  router.get('/', (req, res) => {
+    DataManager.ConnectionGet().then((pins) => {
+      res.json(pins)
+    })
+    .catch((error) => {
+      res.status(500)
+      res.json(error)
+    })
+  })
+  
+  //Updates a pin sent by client
+  router.put('/', (req, res) => {
+    DataManager.ConnectionUpdate(req.body)
+      .then((result) => {
+        res.status(201).json({string:'Successfully updated Connection'})
+      }).catch((error) => {
+        res.status(500)
+        res.json(error)
+      })
+  })
+  
+  //Recieves pin from a client and adds it to the database 
+  router.post('/', (req, res) => {
+    DataManager.ConnectionCreate(req.body)
+      .then((result) => {
+        res.status(201).json({string:'Successfully added Connection'})
+      }).catch((error) => {
+        res.status(500).json(error)
+      })
+  })
+  
+  
+  router.delete('/', (req, res) => {
+    DataManager.ConnectionDelete(req.body)
+      .then((result) => {
+        res.status(200).json({string:'Successfully deleted Connection'})
+      }).catch((error) => {
+        res.status(500)
+        res.json(error)
+      })
+  })
+  
+  module.exports = router;
