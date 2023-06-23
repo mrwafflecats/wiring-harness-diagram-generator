cd81157d254ac78fc20fbed526774b56f9f53352
MrWaffleCats <slanavanh@gmail.com>
2023-06-23 14:14:21 -0500

    created and added boilerplate

---
+++ vue-project/src/components/Connections.vue
@@ -0,0 +1,21 @@
+<script>
+const API_Connections = "http://localhost:4000/connections"
+
+export default{
+
+    data: function(){
+        return {
+            connections: [],
+        }
+    },
+
+    methods:{
+
+    }
+
+}
+</script>
+
+<template>
+    
+</template>
